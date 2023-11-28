19aa1a837f2d2701dadc222ab528d31dce736598
Linas <l.sarpauskas@gmail.com>
2023-11-28 15:02:41 +0200

    Initial commit

---
+++ src/main/java/com/example/LostAndFoundApp/LostAndFoundAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.LostAndFoundApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostAndFoundAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostAndFoundAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LostAndFoundApp/LostAndFoundAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LostAndFoundApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostAndFoundAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
