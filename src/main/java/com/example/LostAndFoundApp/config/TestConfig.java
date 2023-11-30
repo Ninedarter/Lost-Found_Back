0b9a5bab5f80651dcd48b62e3cd1cc2352c22f83
zie2ne <ziedune.matel@gmail.com>
2023-11-30 13:32:20 +0200

    config updated

--- src/main/java/com/example/LostAndFoundApp/config/TestConfig.java
+++ src/main/java/com/example/LostAndFoundApp/config/TestConfig.java
@@ -1,13 +1,13 @@
 package com.example.LostAndFoundApp.config;
 
-import com.example.LostAndFoundApp.auditing.SimpleAuditorAware;
+import com.example.LostAndFoundApp.auditing.ApplicationAuditAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.AuditorAware;
 
 public class TestConfig {
 
     @Bean
-    public AuditorAware<String> auditorAware() {
-        return new SimpleAuditorAware();
+    public AuditorAware<Integer> auditorAware() {
+        return new ApplicationAuditAware();
     }
 }
