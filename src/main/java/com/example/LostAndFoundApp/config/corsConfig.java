cfba4520b2282fc055e2309ead22c7ea24a6188e
Motiejus Slimas <motiejusslim@gmail.com>
2023-11-30 17:26:47 +0200

    Created CORS configuration.

---
+++ src/main/java/com/example/LostAndFoundApp/config/corsConfig.java
@@ -0,0 +1,16 @@
+package com.example.LostAndFoundApp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class corsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**") // Apply CORS for specific paths
+                .allowedOrigins("http://localhost:3000") // Specify allowed origins
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Specify allowed methods
+                .allowedHeaders("*"); // Specify allowed headers
+    }
+}
